56298109ec52adeb8e919ea23b312dad77012ee2
salihersoyleyen-del <salihersoyleyen@gmail.com>
2026-08-28 16:15:05 +0300

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllEquipment } from "@/lib/data";
-import EquipmentList from "@/components/EquipmentList";
-
-export default function Home() {
-  const equipment = getAllEquipment();
-  return <EquipmentList equipment={equipment} />;
-}
