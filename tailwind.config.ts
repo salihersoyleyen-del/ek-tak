27fcbdbe82fd182919a9a07e6063d1820b0dfda3
salihersoyleyen-del <salihersoyleyen@gmail.com>
2026-08-28 16:25:00 +0300

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
